cde789ca0ab22d328131b528ecd0f1d87a3ddbcd
satyam tomar <148446427+satyam-jaat@users.noreply.github.com>
2025-01-22 22:57:50 +0530

    Create DoubleMinMax.java
    define the minimum and maximum value of double in java

---
+++ DoubleMinMax.java
@@ -0,0 +1,10 @@
+public class DoubleMinMax {
+  public static void main(String args[]) {
+    double max = Double.MAX_VALUE;
+    double min = -Double.MAX_VALUE;
+
+    System.out.println(max);
+    System.out.println(min);
+
+  }
+}
